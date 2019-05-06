0658b886fc37000f42f78d1e04b1ada9cfe15d3f
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-06 14:52:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = new Object({keys: 0});
  
-var updatePlaylist(playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
 
-var removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
 }
